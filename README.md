023a36fe3f19b5fb98cc96d3d6d9d1451188b6d8
terminator637 <terminator637@gmail.com>
2019-01-14 10:22:11 +0300

    refactor: Simplify Cleanups struct to alias for slice of functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # gocleanups
 
-This golang module is Dependency Injection helper for cleanup resources.
+This golang module is a helper to cleanup resources.
+
+Inspired by Wire's [Cleanup functions](https://github.com/google/wire/blob/master/docs/guide.md#cleanup-functions)
--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package gocleanups
 
 // Cleanups struct
-type Cleanups struct {
-	funcs []func()
-}
+type Cleanups []func()
 
 // Len returns the number of cleanup functions
-func (c *Cleanups) Len() int {
-	return len(c.funcs)
+func (c Cleanups) Len() int {
+	return len(c)
 }
 
 // Run all Cleanups
-func (c *Cleanups) Run() {
-	for _, f := range c.funcs {
+func (c Cleanups) Run() {
+	for _, f := range c {
 		f()
 	}
 }
@@ -20,22 +18,20 @@ func (c *Cleanups) Run() {
 // RunAndReset runs all Cleanups and resets funcs to empty slice
 func (c *Cleanups) RunAndReset() {
 	c.Run()
-	c.funcs = NewCleanups().funcs
+	*c = NewCleanups()
 }
 
 // Add cleanup funcs to Cleanups
 func (c *Cleanups) Add(funcs ...func()) {
-	c.funcs = append(c.funcs, funcs...)
+	*c = append(*c, funcs...)
 }
 
 // Export Cleanups as simple func
-func (c *Cleanups) Export() func() {
+func (c Cleanups) Export() func() {
 	return c.Run
 }
 
 // NewCleanups constructor
-func NewCleanups(funcs ...func()) *Cleanups {
-	return &Cleanups{
-		funcs: append(make([]func(), 0, len(funcs)), funcs...),
-	}
+func NewCleanups(funcs ...func()) Cleanups {
+	return append(make([]func(), 0, len(funcs)), funcs...)
 }
